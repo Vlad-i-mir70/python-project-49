b79d9dea0386f08f13d59b9239781338fbb6756d
Vladimir Popchenko <pwd70@mail.ru>
2023-01-19 19:46:33 +0300

    add changes in code

--- brain_games/games/gcd/gcd.py
+++ brain_games/games/gcd/gcd.py
@@ -4,7 +4,6 @@ from random import randint
 def gcd():
     number1 = randint(10, 99)
     number2 = randint(10, 99)
-    #print(f' numb1 {number1}')
     print(f'Question: {number1} {number2}')
     i = number1 + 1
     while 1 < i <= number1 + 1:

2fdbc2b4403021753701bc7e1b819c11d2842db9
Vladimir Popchenko <pwd70@mail.ru>
2023-01-23 17:34:36 +0300

    add even dir

---
+++ brain_games/games/even/even.py
@@ -0,0 +1,10 @@
+from random import randint
+
+
+def brain_even():
+    number = randint(1, 1000)
+    print(f'Question: {number}')
+    if number % 2  == 0:
+        return 'yes'
+
+    return 'no'

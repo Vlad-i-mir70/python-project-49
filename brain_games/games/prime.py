c3c9bd59cd6d4b89bf245e3de5de7eeebb9b8243
Vladimir Popchenko <pwd70@mail.ru>
2023-02-01 21:53:56 +0300

    check old code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,25 +1,20 @@
 from random import randint
-
-
-def is_nonprime(number):
-    if number == 1:
-        return number == 1
-    else:
-        for i in range(2, number):
-            if number % i == 0:
-                return number % i == 0
-            else:
-                return number % i == 0
+from math import sqrt
 
 
 def brain_prime():
     number = randint(1, 1000)
     task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
     question = f' Question: {number}'
-    answer = is_nonprime(number)
-    if answer:
-        result = 'no'
-        return result, task, question
-    else:
-        result = 'yes'
-        return result, task, question
+
+    def is_prime(number):
+        i = 1
+        if number == 3:
+            return 'yes'
+        while i < sqrt(number):
+            i += 1
+            if number % i == 0:
+                return 'no'
+        return 'yes'
+    result = is_prime(number)
+    return result, task, question

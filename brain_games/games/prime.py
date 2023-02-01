1c37d09852725ba34898d3817ca885b0b021895b
Vladimir Popchenko <pwd70@mail.ru>
2023-02-01 23:34:06 +0300

    change code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,19 +2,25 @@ from random import randint
 from math import sqrt
 
 
+def is_prime(number):
+    if number == 2:
+        return number == 2
+    i = 1
+    while i < sqrt(number):
+        i += 1
+        if number % i == 0:
+            return number % i != 0
+        return number % i != 0
+
+
 def brain_prime():
     number = randint(1, 1000)
     task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
     question = f' Question: {number}'
-
-    def is_prime(number):
-        i = 1
-        if number == 3:
-            return 'yes'
-        while i < sqrt(number):
-            i += 1
-            if number % i == 0:
-                return 'no'
-        return 'yes'
-    result = is_prime(number)
-    return result, task, question
+    answer = is_prime(number)
+    if answer:
+        result = 'yes'
+        return result, task, question
+    else:
+        result = 'no'
+        return result, task, question

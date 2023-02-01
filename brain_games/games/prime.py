81736708b0eb261ee441af043d2693266d2be2ec
Vladimir Popchenko <pwd70@mail.ru>
2023-02-01 21:13:26 +0300

    change code

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -16,7 +16,6 @@ def brain_prime():
     number = randint(1, 1000)
     task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
     question = f' Question: {number}'
-
     answer = is_nonprime(number)
     if answer:
         result = 'no'
